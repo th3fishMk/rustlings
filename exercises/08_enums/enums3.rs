90191efc83db570ec0f4f9520f8189da74be5f9d
fish <th3fishmk@proton.me>
2026-07-31 07:16:01 -0600

    solving 11

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -44,8 +44,19 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: Create a match expression to process the different message
-        // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => {
+                self.resize(width, height);
+            }
+            Message::Move(p) => self.move_position(p),
+            Message::Echo(msg) => {
+                self.echo(msg);
+            }
+            Message::ChangeColor(red, green, blue) => {
+                self.change_color(red, green, blue);
+            }
+            Message::Quit => self.quit(),
+        }
     }
 }
 
