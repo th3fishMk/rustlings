d4a2b0817ea4b90fbbead76fe7efa16e75c82687
fish <th3fishmk@proton.me>
2026-08-06 13:35:50 -0600

    solving up to generics

--- exercises/13_error_handling/errors6.rs
+++ exercises/13_error_handling/errors6.rs
@@ -24,8 +24,9 @@ impl ParsePosNonzeroError {
         Self::Creation(err)
     }
 
-    // TODO: Add another error conversion function here.
-    // fn from_parse_int(???) -> Self { ??? }
+    fn from_parse_int(err: ParseIntError) -> Self {
+        Self::ParseInt(err)
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -41,10 +42,11 @@ impl PositiveNonzeroInteger {
     }
 
     fn parse(s: &str) -> Result<Self, ParsePosNonzeroError> {
-        // TODO: change this to return an appropriate error instead of panicking
-        // when `parse()` returns an error.
-        let x: i64 = s.parse().unwrap();
-        Self::new(x).map_err(ParsePosNonzeroError::from_creation)
+        let x = s.parse();
+        match x {
+            Ok(x) => Self::new(x).map_err(ParsePosNonzeroError::from_creation),
+            Err(err) => Err(ParsePosNonzeroError::ParseInt(err)),
+        }
     }
 }
 
