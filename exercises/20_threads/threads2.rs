a3596861febb256e8315ce233b59b553c380c567
fish <th3fishmk@proton.me>
2026-08-08 02:54:41 -0600

    solved clippy

--- exercises/20_threads/threads2.rs
+++ exercises/20_threads/threads2.rs
@@ -2,15 +2,20 @@
 // work. But this time, the spawned threads need to be in charge of updating a
 // shared value: `JobStatus.jobs_done`
 
-use std::{sync::Arc, thread, time::Duration};
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
+#[derive(Debug)]
 struct JobStatus {
     jobs_done: u32,
 }
 
 fn main() {
     // TODO: `Arc` isn't enough if you want a **mutable** shared state.
-    let status = Arc::new(JobStatus { jobs_done: 0 });
+    let status = Arc::new(Mutex::new(JobStatus { jobs_done: 0 }));
 
     let mut handles = Vec::new();
     for _ in 0..10 {
@@ -18,8 +23,8 @@ fn main() {
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
 
-            // TODO: You must take an action before you update a shared value.
-            status_shared.jobs_done += 1;
+            let mut m = status_shared.lock().unwrap();
+            m.jobs_done += 1;
         });
         handles.push(handle);
     }
@@ -30,5 +35,5 @@ fn main() {
     }
 
     // TODO: Print the value of `JobStatus.jobs_done`.
-    println!("Jobs done: {}", todo!());
+    println!("Jobs done: {:?}", *status.lock().unwrap());
 }
